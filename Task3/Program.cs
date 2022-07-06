8300a0307a7deed5f74c88a050948eb0f76a6497
StenKa <stepansergeevichz13@gmail.com>
2022-07-07 01:01:48 +0300

    Выполнена третья задача

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число от 1 до 7, где 1 - это понедельник, 2 - вторник ... 7 - воскресенье: ");
+int input = Convert.ToInt32(Console.ReadLine());
+if (input > 5){
+    Console.WriteLine("Это выходной день");
+}
+else {
+    Console.WriteLine("Это будний день");
+}
